b86482604a5b1f77e68fec027113b50d99f19ddc
g60080zu <09zu01@gmail.com>
2025-04-14 20:27:50 +0100

    sync push

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,19 @@
-console.log("popup.js loaded");
-
-document.getElementById("close-btn").addEventListener("click", () => {
-  window.close(); // Closes the popup window
-});
+document.getElementById("close-btn").addEventListener("click", () => { window.close(); });
 
 document.getElementById("scan").addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
       if (tab?.id) {
-        console.log("clicked");
-        chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ["content.js"]
-        });
+        chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ["content.js"]});
       }
     });
+  });
+
+  document.getElementById("ai-tab").addEventListener("click", () => {
+    document.getElementById("scan-tab-content").style.display = "none";
+    document.getElementById("ai-tab-content").style.display = "block";
+  });
+  
+  document.getElementById("scan-tab").addEventListener("click", () => {
+    document.getElementById("ai-tab-content").style.display = "none";
+    document.getElementById("scan-tab-content").style.display = "block";
   });
