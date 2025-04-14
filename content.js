7ee1ecca167d5c272e0ecab7309ca86d68b8fe50
g60080zu <09zu01@gmail.com>
2025-04-14 17:36:15 +0100

    Basic content code

---
+++ content.js
@@ -0,0 +1,27 @@
+fetch(chrome.runtime.getURL("data/bias_words.json"))
+  .then(res => res.json())
+  .then(biasWords => {
+    const walk = (node) => {
+      if (node.nodeType === 3) {
+        const original = node.nodeValue;
+        const replaced = original.replace(
+          new RegExp(`\\b(${Object.keys(biasWords).join("|")})\\b`, "gi"),
+          (match) => {
+            const replacement = biasWords[match.toLowerCase()];
+            return `<span class="bias-highlight" title="Try: ${replacement}">${match}</span>`;
+          }
+        );
+        if (original !== replaced) {
+          const span = document.createElement("span");
+          span.innerHTML = replaced;
+          node.parentNode.replaceChild(span, node);
+        }
+      } else {
+        for (let child of node.childNodes) {
+          walk(child);
+        }
+      }
+    };
+
+    walk(document.body);
+  });
